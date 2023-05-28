64e5c1bb4764760d87b11acf03ec5f6ee5a3b305
Andrey Morozov <pisemest@yandex.ru>
2023-05-28 19:15:29 +0500

    add 2 themes and global variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useContext} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 
-import './index.scss'
+import './styles/index.scss'
 import {Counter} from "./components/Counter";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme()
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
