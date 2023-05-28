7ce47033bc05139489bc1398049e20e3d60b9555
Andrey Morozov <pisemest@yandex.ru>
2023-05-28 16:13:24 +0500

    add lazy loading pages

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,9 @@
+const AboutPage = (): JSX.Element => {
+    return (
+        <div>
+            ABOUT PAGE
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+const MainPage = (): JSX.Element => {
+    return (
+        <div>
+            MAIN PAGE
+        </div>
+    );
+};
+
+export default MainPage;
