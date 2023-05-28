59271169e9bf72ff05f3fc30cdc9f111cb0b4ad8
Andrey Morozov <pisemest@yandex.ru>
2023-05-28 19:33:39 +0500

    add class names function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React, {Suspense, useContext} from 'react';
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 
 import './styles/index.scss'
 import {Counter} from "./components/Counter";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {Theme, ThemeContext} from "./theme/ThemeContext";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
-    const { theme, toggleTheme } = useTheme()
-
+    const {theme, toggleTheme} = useTheme()
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
