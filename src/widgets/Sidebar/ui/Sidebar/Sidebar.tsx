f953b30592263d3fb5bac4df0e70a2f06e81cdc0
Andrey Morozov <pisemest@yandex.ru>
2023-06-06 11:52:25 +0500

    fix toggle btn

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -11,15 +12,15 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false);
+  const { t } = useTranslation();
 
   const onToggle = () => {
-    setCollapsed(prevState => !prevState);
+    setCollapsed((prevState) => !prevState);
   };
   return (
     <div data-testid="sidebar" className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-      {/* eslint-disable-next-line i18next/no-literal-string */}
       <Button data-testid="sidebar-toggle" onClick={onToggle}>
-        toggle
+        {t('Переключить')}
       </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
